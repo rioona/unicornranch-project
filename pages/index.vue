79d7211df96bdd2c2332767a7a5fed57023af0d9
rnpark@unicornranch.co.kr <>
2022-09-30 00:36:39 +0900

    first commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,199 @@
 <template>
-  
+    <div class="wrap">
+        <header>
+            <div class="inner">
+                <div class="left">
+                    <a href="" class="logo">UnicornRanch</a>
+                    <nav class="gnb">
+                        <a href="">회사소개</a>
+                        <a href="">서비스</a>
+                        <a href="">뉴스/공지</a>
+                        <a href="">고객지원</a>
+                    </nav>
+                </div>
+                <div class="right">
+                    <button type="button">KR</button>
+                    <ul>
+                        <li>KR</li>
+                        <li>EN</li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+        <div class="main">
+            <div class="slideshow-container">
+                <div class="slideshow-wrapper">
+                    <div class="slide">
+                        <h3>About</h3>
+                        <h2>Unicorn Ranch</h2>
+                        <div class="s_text">
+                            유니콘랜치는 1999년 설립된 위즈도메인의 자회사로서 20여 년간 축적된 특허기반의<br>
+                            기업 기술정보를 바탕으로 국내 8만 비상장 데이터 및 국내/해외 상장 데이터를 분석하여,<br>
+                            금융소비자 및 엔젤투자자에게 우수기업을 연결시켜 기업에게는 자금 유치를 통한 기업 성장을<br>
+                            금융소비자에게는 정확한 기술정보제공 및 우량기술기업에 대한 투자의 기회를 제공하며<br>
+                            기업과 사용자 모두에게 선순환 투자 시스템을 조성합니다.</div>
+                    </div>
+                    <div class="slide">
+                        <h3>Service</h3>
+                        <div class="f_title">10Bagger</div>
+                        <div>Techinfo Platform</div>
+                        <div class="s_text">
+                            텐배거에서 제공되는 정보는 특허기반 밸류에이션을 산출하여 성장가능성 높은<br>
+                            상장/비상장 기술 종목을 발굴하고 모든 기술 기업의<br>
+                            데이터정보서비스, 섹터/테마랭킹, 퀀트 종목추천,  AI 매매신호, 기업가치분석리포트 정보를<br>
+                            증권사등의 기관에 DATA FEED, SaaS, ASP 서비스 형태로 제공합니다.
+                        </div>
+                    </div>
+                    <div class="slide">
+                        <h3>Service</h3>
+                        <div class="f_title">10Bagger</div>
+                        <div>Techinfo Platform</div>
+                        <div class="s_text">
+                            텐배거 트레이딩 서비스는 가상 자산에 대한 거래 기반으로 구성되었습니다.<br>
+                            이를 기반으로 국내 비상장 기업의 주식거래를 기본 가능으로 증권사등의 기관에<br>
+                            OpenAPI 형태의 SaaS 서비스를 제공합니다.<br>
+                            또한, 다양한 형태의 가상 자산을 거래할 수 있는 기능을<br>
+                            SaaS, ASP 서비스 형태로 제공합니다.
+                        </div>
+                    </div>
+                </div>
+                <div class="pagination">
+                    <a href="#"></a>
+                    <a href="#"></a>
+                    <a href="#"></a>
+                </div>
+                <button type="button">이전</button>
+                <button type="button">다음</button>
+            </div>
+            <section class="area1">
+                <div class="inner">
+                    <article class="m_info">
+                        <h2>About<br>Unicorn Ranch</h2>
+                        <p>기술중심 정보, 거래 플랫폼 서비스를 제공</p> 
+                        <div class="btns">
+                            <a href="">연혁 및 비전</a>
+                        </div>
+                    </article>
+                    <article>
+                        <div class="box techinfo">
+                            <div class="box_inner">
+                                <span class="Techinfo_logo"><h3>10Bagger Techinfo Platform</h3></span>
+                                <div class="text">
+                                    <span>SERVICE</span>
+                                    <h3>10Bagger Techinfo Platform</h3>
+                                    <p>상장/비상장 종목에 대한 기업가치분석 정보 제공합니다.</p>
+                                </div>
+                                <a href="" class="btn_link">자세히 보기</a>
+                            </div>
+                        </div>
+                        <div class="box u_trading">
+                            <div class="box_inner">
+                                <span class="Techinfo_logo"><h3>10Bagger Techinfo Platform</h3></span>
+                                <div class="text">
+                                    <span>SERVICE</span>
+                                    <h3>10Bagger Techinfo Platform</h3>
+                                    <p>상장/비상장 종목에 대한 기업가치분석 정보 제공합니다.</p>
+                                </div>
+                                <a href="" class="btn_link">자세히 보기</a>
+                            </div>
+                        </div>
+                    </article>
+                </div>
+            </section>
+            <section class="area2">
+                <div class="inner">
+                    <article class="notice">
+                        <h2>News &amp; Notice</h2> 
+                        <ul class="list">
+                            <li>
+                                <a href="" target="_blank">
+                                    <div><span>01</span>2022.01</div>
+                                    <div><p>유니콘랜치 텐배거 서비스 베타버전 오픈</p></div>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="" target="_blank">
+                                    <div><span>01</span>2022.01</div>
+                                    <div><p>유니콘랜치 텐배거 서비스 베타버전 오픈</p></div>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="" target="_blank">
+                                    <div><span>01</span>2022.01</div>
+                                    <div><p>유니콘랜치 텐배거 서비스 베타버전 오픈</p></div>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="" target="_blank">
+                                    <div><span>01</span>2022.01</div>
+                                    <div><p>유니콘랜치 텐배거 서비스 베타버전 오픈</p></div>
+                                </a>
+                            </li>    
+                        </ul>
+                    </article>
+                    <article>
+                        <h2>Partner &amp; Contact us</h2>
+                            <div class="box2">
+                                <div class="box">
+                                    <div class="box_inner">
+                                        <h2>업무제휴 문의</h2>
+                                        <div class="t1">
+                                            <p>텐배거상장<br>stock@unicornranch.co.kr</p>
+                                            <a href="">문의하기</a>
+                                        </div>
+                                        <div class="t2">
+                                            <p>텐배거비상장<br>unlistedstock@unicornranch.co.kr</p>
+                                            <a href="">문의하기</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="box">
+                                    <div class="box_inner">
+                                        <h2>온라인 문의</h2>
+                                        <p>온라인을 통한 <br>고객문의를 지원합니다.<br>최대한 빠른 서비스로<br>응대합니다.</p>
+                                        <a href="">바로가기</a>
+                                    </div>
+                                </div>
+                            </div>
+                    </article>
+                </div>
+            </section>
+        </div>
+        <footer>
+            <div class="inner">
+                <div class="f_left">
+                    <div class="logo">Unicorn Ranch</div>
+                </div>
+                <div class="f_right">
+                    <div class="top">
+                        <div class="top_l">
+                            <a href="">개인정보 처리방침</a>
+                            <a href="">이메일무단수집거부</a>
+                        </div>
+                        <div class="top_r">
+                            <div class="top_tit">Family Site</div>
+                            <ul>
+                                <li>위즈도메인</li>
+                                <li>아이피밸류 파트너스</li>
+                                <li>KTCB</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="biz">
+                        <div class="biz_l">
+                            <span>서울특별시 영등포구 여의대로 108 파크원1 5층</span> 
+                            <span>사업자등록번호 : 698-87-02015</span> 
+                            <strong>Copyright © UnicornRanch. All Rights Reserved.</strong>
+                        </div> 
+                        <div class="biz_r">
+                            <span>대표전화<strong>02.2088.1271</strong></span> 
+                            <span>펙스<strong>02.2118.0175</strong></span>
+                        </div> 
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
 </template>
 
 <script>
