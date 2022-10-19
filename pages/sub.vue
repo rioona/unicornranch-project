4e120cde7f04c01b16a56998591919c27d17933e
rnpark@unicornranch.co.kr <>
2022-10-20 08:39:59 +0900

    sub.vue 추가

---
+++ pages/sub.vue
@@ -0,0 +1,104 @@
+<template>
+    <div class="wrap">
+        <header>
+            <div class="inner">
+                <div class="left">
+                    <a href="" class="logo">UnicornRanch</a>
+                    <nav class="gnb">
+                        <a href="">회사소개</a>
+                        <a href="">서비스</a>
+                        <a href="">뉴스/공지</a>
+                        <a href="">고객지원</a>
+                    </nav>
+                </div>
+                <div class="right">
+                    <div class="search"></div>
+                    <div class="l_icon"></div>
+                    <button type="button" class="hr_tit">KR</button>
+                    <div class="r_icon"></div>
+                    <ul>
+                        <li><button type="button">KR</button></li>
+                        <li><button type="button">EN</button></li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+        <div class="main">
+            <div class="top_visual">
+                <div class="inner">
+                    <h1>NEWS &amp; Notice</h1>
+                    <p>기업기술가치 전문기업 (주)유니콘랜치의 기업 관련 뉴스입니다.</p>
+                </div>
+            </div>
+            <div class="subnev">
+                <div class="inner">
+                    <div class="sn_box"></div>
+                    <div class="sn_box">
+                        <div class="sn_tit">
+                            뉴스/공지
+                        </div>
+                        <ul class="slct_cont">
+                            <li>
+                                <NuxtLink to="" target="_blank"></NuxtLink>
+                            </li>
+                            <li>
+                                <NuxtLink to="" target="_blank"></NuxtLink>
+                            </li>
+                            <li>
+                                <NuxtLink to="" target="_blank"></NuxtLink>
+                            </li>
+                            <li>
+                                <NuxtLink to="" target="_blank"></NuxtLink>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="sn_box"></div>
+                </div>
+            </div>
+        </div>
+        <footer>
+            <div class="inner">
+                <div class="f_left">
+                    <div class="logo">Unicorn Ranch</div>
+                </div>
+                <div class="f_right">
+                    <div class="top">
+                        <div class="top_l">
+                            <a href="">개인정보 처리방침</a>
+                            <a href="">이메일무단수집거부</a>
+                        </div>
+                        <div class="top_r">
+                            <div class="top_tit">Family Site</div>
+                            <ul>
+                                <li>텐베거 테크인포 플랫폼</li>
+                                <li>텐베거 트레이딩 플랫폼</li>
+                                <hr>
+                                <li>아이피밸류 파트너스</li>
+                                <li>오픈아이피</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="biz">
+                        <div class="biz_l">
+                            <span>서울특별시 영등포구 여의대로 108 파크원1 5층</span> 
+                            <span>사업자등록번호 : 698-87-02015</span> 
+                            <strong>Copyright © UnicornRanch. All Rights Reserved.</strong>
+                        </div> 
+                        <div class="biz_r">
+                            <span>대표전화<strong>02.2088.1271</strong></span> 
+                            <span>팩스<strong>02.2118.0175</strong></span>
+                        </div> 
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+@import "@/static/scss/style.scss";
+</style>
